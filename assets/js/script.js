d1e32e6ca4a8bd67aaa40bd46edc53a0e35ab019
KellyT4425 <k.thompson4421@yahoo.com>
2025-03-13 14:49:10 +0000

    html file and structure added, css style sheet links to html file, along with js script file. Basic styling added to css

---
+++ assets/js/script.js
@@ -0,0 +1,74 @@
+
+/**
+ * function to add how the game should run
+ */
+function runGame() {
+
+}
+
+function duplicateIcons() {
+    let myIcons = [
+        assets/images/card1.png,
+        assets/images/card2.png,
+        assets/images/card3.png,
+        assets/images/card4.png,
+        assets/images/card5.png,
+        assets/images/card6.png,
+        assets/images/card7.png,
+        assets/images/card8.png,
+        assets/images/card9.png,
+        assets/images/card10.png
+    ];
+
+    let duplicate = myIcons * myIcons;
+    console.log(duplicate);
+}
+
+duplicateIcons();
+
+
+/**
+ * function to shuffle cards and duplicate card array,
+ */
+function cardShuffle() {
+let cards = document.getElementsByClassName("card");
+
+}
+
+
+
+/**
+ * function to make card flip when clicked to display, icon.
+ */
+function flipCard() {
+
+}
+
+/**
+ * function to determine when cards are matched, and cards disappear, score is incremented.
+ */
+function cardMatched() {
+
+}
+
+
+/**
+ * function if there is no match, include hiding cards again
+ */
+function noMatch() {
+
+}
+
+/**
+ * function to increment score on matched cards.
+ */
+function matchScore() {
+
+}
+
+/**
+ * shuffle button reset game.
+ */
+function shuffleReset() {
+
+}
