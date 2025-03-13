f13568bf464d0e7bc0110ff0e92cd56055d5b9c2
KellyT4425 <k.thompson4421@yahoo.com>
2025-03-13 22:05:09 +0000

    card-back, card-inner added to html structure to help with flip card function, arrays created for cards and icons to script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,21 @@
+document.addEventListener("DOMContentLoaded", runGame);
 
 /**
  * function to add how the game should run
  */
 function runGame() {
+  let cardsFront = document.getElementsByClassName("card");
 
+  for (let card of cardsFront) {
+    card.addEventListener("click", flipCard);
+  }
 }
 
-
 /**
- * duplicate icon array to may 10 pairs.
+ * duplicate icon array to make 10 pairs.
  */
 function duplicateIcons() {
-let myIcons = [
+  let myIcons = [
     "assets/images/card1.png",
     "assets/images/card2.png",
     "assets/images/card3.png",
@@ -21,66 +25,64 @@ let myIcons = [
     "assets/images/card7.png",
     "assets/images/card8.png",
     "assets/images/card9.png",
-    "assets/images/card10.png"
-];
+    "assets/images/card10.png",
+  ];
 
-    let duplicate = [...myIcons, ...myIcons];
-    let randomIcon = Math.floor(Math.random() * duplicate.length);
-    console.log(randomIcon);
-   
+  let duplicate = [...myIcons, ...myIcons];
+  return duplicate;
 }
 
 duplicateIcons();
 
-
 /**
- * function to shuffle cards and assign icon to cards
+ * function the shuffle cards and assign icon to cards
  */
 function cardShuffle() {
-let cards = document.getElementsByClassName("card");
-
-
-
-
-
-}
+    const iconArray = duplicateIcons();
+    const cardBacks = document.getElementsByClassName("card-back");
+  
+    for (let i = 0; i < iconArray.length; i++) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [iconArray[i], iconArray[j]] = [iconArray[j], iconArray[i]];
+    }
+  /**
+   * Iterates throught cardBacks array and assigns to card-back <div>
+   */
+    for (let i = 0; i < cardBacks.length; i++) {
+      cardBacks[i].innerHTML = `<img src="${iconArray[i]}" alt="icon">`;
+    }
+  
+    console.log(iconArray);
+    console.log(cardBacks);
+  }
 
 cardShuffle();
-
-
 
 /**
  * function to make card flip when clicked to display, icon.
  */
 function flipCard() {
-
+  cardsFront = document.getElementsByClassName("card");
+  
 }
+
 
 /**
  * function to determine when cards are matched, and cards disappear, score is incremented.
  */
-function cardMatched() {
-
-}
-
+function cardMatched() {}
 
 /**
  * function if there is no match, include hiding cards again
  */
-function noMatch() {
-
-}
+function noMatch() {}
 
 /**
  * function to increment score on matched cards.
  */
-function matchScore() {
-
-}
+function matchScore() {}
 
 /**
  * shuffle button reset game.
  */
-function shuffleReset() {
-
-}
+function shuffleReset() {}
